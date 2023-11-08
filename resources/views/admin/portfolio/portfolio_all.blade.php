73ac919e45c180cb78aa8de939cf1f492f1e1aee
vikmaamaa <maamaavictor@yahoo.com>
2023-11-08 17:38:25 +0100

    feat: portfolio page update

--- resources/views/admin/portfolio/portfolio_all.blade.php
+++ resources/views/admin/portfolio/portfolio_all.blade.php
@@ -44,14 +44,14 @@
                                 <tr>
                                     <td>{{ $i++ }}</td>
                                     <td>{{ $item->portfolio_name}}</td>
-                                    <td>{{ $item->portfolio_image}}</td>
+                                    <td>{{ $item->portfolio_title}}</td>
                                     <td> <img src="{{ asset($item->portfolio_image) }}" alt="" style="width: 60px; height: 50px"> </td>
                                     <td>
-                                        <a href="{{ route('edit.multi.image', $item->id) }}" class="btn btn-info sm" title="Edit Data">
+                                        <a href="{{ route('edit.portfolio', $item->id) }}" class="btn btn-info sm" title="Edit Data">
                                             <i class="fas fa-edit"></i>
                                         </a>
 
-                                        <a href="{{ route('delete.multi.image', $item->id) }}" class="btn btn-danger sm" title="Delete Data" id="delete">
+                                        <a  class="btn btn-danger sm" title="Delete Data" id="delete">
                                             <i class="fas fa-trash-alt"></i>
                                         </a>
                                     </td>
