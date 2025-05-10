af6dce1247853d6fb0f2ccea7c2824e1f6d09628
vikmaamaa <nwisuanu@gmail.com>
2025-05-10 21:07:04 +0100

    feat: backend contact form page setup

--- app/Http/Controllers/Home/ContactController.php
+++ app/Http/Controllers/Home/ContactController.php
@@ -31,4 +31,21 @@ class ContactController extends Controller
 
         return redirect()->back()->with($notifications);
     }//end method
+
+    public function ContactMessage() {
+        $contacts = Contact::latest()->get();
+
+        return view('admin.contact.allcontact', compact('contacts'));
+    }//end method
+
+    public function DeleteMessage($id) {
+        Contact::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Your Message Deleted Successfullly',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }//end method
 }
